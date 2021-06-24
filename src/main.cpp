5a7f7fdc629ee758fad6a427ca8f84ec3ed7e935
Luis Chiang <lahoising78@gmail.com>
2021-06-24 00:38:58 -0400

    clean main

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    Mat4 mat{};
-
-    std::cout << mat << std::endl;
-    std::cout << "mat scaled {0.5, 1.7, 89.0} = " << mat.Scale({0.5f, 1.7f, 89.0f}) << std::endl;
-    std::cout << "mat translated {7.0, 23.0, 98.17} = " << mat.Translate({7.0f, 23.0f, 98.17f}) << std::endl;
+    
 
     return 0;
 }
