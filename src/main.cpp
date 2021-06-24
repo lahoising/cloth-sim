d3491f61555d45c938273b1d35ec2bc677dee176
Luis Chiang <lahoising78@gmail.com>
2021-06-24 00:47:51 -0400

    add glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 #include "math/vec.h"
 #include "math/mat4.h"
 
 int main(int argc, char *argv[])
 {
-    
-
+    glfwInit();
+    glfwTerminate();
     return 0;
 }
