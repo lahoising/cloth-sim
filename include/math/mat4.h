d73fce5b4b0d21c915b5a2b93871607fb2744273
Luis Chiang <lahoising78@gmail.com>
2021-06-24 00:11:07 -0400

    mat4 default constructor

---
+++ include/math/mat4.h
@@ -0,0 +1,12 @@
+#ifndef _MAT4_H_
+#define _MAT4_H_
+
+#include "matrix.h"
+
+class Mat4 : public Mat<4,4>
+{
+public:
+    Mat4() : Mat<4,4>() {}
+};
+
+#endif
---
+++ include/math/matrix.h
@@ -0,0 +1,39 @@
+#ifndef _MATRIX_H_
+#define _MATRIX_H_
+
+#include <inttypes.h>
+#include <iostream>
+
+/// @note stored as array of column vectors
+template<uint32_t RowCount, uint32_t ColCount>
+class Mat
+{
+public:
+    Mat()
+    {
+        memset(matrix, 0, sizeof(float) * RowCount * ColCount);
+        for(uint32_t i = 0; i < RowCount; i++)
+        {
+            matrix[i * ColCount + i] = 1.0f;
+        }
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Mat &mat)
+    {
+        os << "[";
+        for(uint32_t col = 0; col < ColCount; col++)
+        {
+            for(uint32_t row = 0; row < RowCount; row++)
+            {
+                os << mat.matrix[ col * RowCount + row ];
+            }
+            os << ";";
+        }
+        return os << "]";
+    }
+
+public:
+    float matrix[ ColCount * RowCount ] = {};
+};
+
+#endif
